60624bb82b2281b15a27ec65dc4dccdfd3463ad4
TransformersWsz <3287124026@qq.com>
2022-04-24 17:54:40 +0800

    update deploy

--- run.sh
+++ run.sh
@@ -5,3 +5,7 @@
 git add .
 git commit -m ${1}
 git push -u origin main
+
+cp correct/hexo-renderer-kramed/renderer.js node_modules/hexo-renderer-kramed/lib/
+cp correct/kramed/inline.js node_modules/kramed/lib/rules/ 
+cp correct/hexo-renderer-mathjax/mathjax.html node_modules/hexo-renderer-mathjax
