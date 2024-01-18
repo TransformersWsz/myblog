cf28cba531ea19ef85c91078126cc582cf57886d
Swift <3287124026@qq.com>
2024-01-18 16:52:03 +0000

    update

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 git add .
 git commit -m "update"
 git push -u origin main
-if [[ $? -ne 0 ]]
+if [[ $? -eq 0 ]]
 then
-    echo "push to github.com failed!"
-else
     echo "push to github.com success!"
+else
+    echo "push to github.com failed!"
 fi
