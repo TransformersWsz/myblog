39a8044cff0b4b3a22ca7296b48e4b72e19332f7
TransformersWsz <3287124026@qq.com>
2024-02-29 02:45:38 +0800

    update

--- themes/keep/scripts/helpers/export-config.js
+++ themes/keep/scripts/helpers/export-config.js
@@ -1,57 +1,53 @@
 /* global hexo */
 
-'use strict';
+'use strict'
 
-const url = require('url');
-const fs = require('fs');
-const path = require('path');
-const yaml = require('js-yaml');
+const url = require('url')
+const fs = require('fs')
+const path = require('path')
+const yaml = require('js-yaml')
 
 /**
  * Export theme config to js
  */
-hexo.extend.helper.register('export_config', function () {
+hexo.extend.helper.register('exportThemeConfig', function () {
+  const { config, theme } = this
 
-  let {config, theme} = this;
-
-  // ------------------------ export language to js ------------------------
-  const languageDir = path.join(__dirname, '../../languages');
-  let file = fs.readdirSync(languageDir).find(v => v === `${config.language}.yml`);
-  file = languageDir + '/' + (file ? file : 'en.yml');
-  let languageContent = fs.readFileSync(file, 'utf8');
+  // -------------------------- export languages --------------------------
+  const languageDir = path.join(__dirname, '../../languages')
+  let file = fs.readdirSync(languageDir).find((v) => v === `${config.language}.yml`)
+  file = languageDir + '/' + (file ? file : 'en.yml')
+  let languageContent = fs.readFileSync(file, 'utf8')
   try {
-    languageContent = yaml.load(languageContent);
-  } catch (e) {
-    console.log(e);
+    languageContent = yaml.load(languageContent)
+  } catch (err) {
+    console.log(err)
   }
   // -----------------------------------------------------------------------
 
-
-  let hexo_config = {
+  // ----------------------------- hexo config -----------------------------
+  const hexoConfig = {
     hostname: url.parse(config.url).hostname || config.url,
-    root: config.root,
+    root: config?.root || '/',
     language: config.language
-  };
+  }
 
   if (config.search) {
-    hexo_config.path = config.search.path;
+    hexoConfig.path = config.search.path
   }
+  // -----------------------------------------------------------------------
 
-  let theme_config = {
-    toc: theme.toc,
-    style: theme.style,
-    local_search: theme.local_search,
-    code_copy: theme.code_copy,
-    side_tools: theme.side_tools,
-    pjax: theme.pjax,
-    lazyload: theme.lazyload,
-    version: theme.version,
-  }
+  // ----------------------------- theme config ----------------------------
+  const themeConfig = theme
+  themeConfig.version = require('../../package.json').version
+  // -----------------------------------------------------------------------
 
-  return `<script id="hexo-configurations">
-    let KEEP = window.KEEP || {};
-    KEEP.hexo_config = ${JSON.stringify(hexo_config)};
-    KEEP.theme_config = ${JSON.stringify(theme_config)};
-    KEEP.language_ago = ${JSON.stringify(languageContent['ago'])};
-  </script>`;
-});
+  return `<script class="keep-theme-configurations">
+    const KEEP = window.KEEP || {}
+    KEEP.hexo_config = ${JSON.stringify(hexoConfig)}
+    KEEP.theme_config = ${JSON.stringify(themeConfig)}
+    KEEP.language_ago = ${JSON.stringify(languageContent['ago'])}
+    KEEP.language_code_block = ${JSON.stringify(languageContent['code_block'])}
+    KEEP.language_copy_copyright = ${JSON.stringify(languageContent['copy_copyright'])}
+  </script>`
+})
