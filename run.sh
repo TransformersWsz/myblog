8ff38bb432ea67bc161ad6f29635405fb07ede45
Swift <3287124026@qq.com>
2024-02-29 08:47:08 +0000

    update

--- run.sh
+++ run.sh
@@ -18,8 +18,9 @@ git push -u origin main
 echo "----------------------------------------------------------------------------"
 if [[ $? -eq 0 ]]
 then
-    echo -e "\033[32mpush to https://github.com/TransformersWsz/myblog success!\033[0m"
+    echo -e "\033[32m\033]8;;https://github.com/TransformersWsz/myblog\apush to https://github.com/TransformersWsz/myblog success~\033]8;;\a\033[0m"
+
 else
-    echo -e "\033[31mpush to https://github.com/TransformersWsz/myblog failed!\033[0m"
+    echo -e "\033[32m\033]8;;https://github.com/TransformersWsz/myblog\apush to https://github.com/TransformersWsz/myblog failed~\033]8;;\a\033[0m"
 fi
 echo "----------------------------------------------------------------------------"
